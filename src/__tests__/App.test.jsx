6f5383d1bffa474524ec85a0cf8af988a20bed17
ShreyasBhagat <shreyasbhagat98@gmail.com>
2025-05-28 20:50:12 +0530

    Added the Test into the files-1

--- src/__tests__/App.test.jsx
+++ src/__tests__/App.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import App from '../App' // Your main React component
 
@@ -12,4 +12,7 @@ test('Increments count on button click', () => {
   render(<App />)
   const buttonElement = screen.getByText(/count is 0/i);
   expect(buttonElement).toBeInTheDocument()
+
+  fireEvent.click(buttonElement);
+  expect(buttonElement).toHaveTextContent('count is 1');
 });
