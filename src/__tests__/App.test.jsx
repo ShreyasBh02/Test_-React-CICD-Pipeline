66f0159a2e341f40e7889b88ca6f2fe474d8e9a9
ShreyasBhagat <shreyasbhagat98@gmail.com>
2025-05-28 20:43:50 +0530

    Added the Test into the files

---
+++ src/__tests__/App.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import App from '../App' // Your main React component
+
+test('renders the heading', () => {
+  render(<App />)
+  const heading = screen.getByText(/Vite \+ React/i)
+  expect(heading).toBeInTheDocument()
+});
+
+test('Increments count on button click', () => {
+  render(<App />)
+  const buttonElement = screen.getByText(/count is 0/i);
+  expect(buttonElement).toBeInTheDocument()
+});
